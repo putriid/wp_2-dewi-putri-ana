5b5d28d8adedbd10b1271bfb324308285d9ade24
putriid <deputriana20@gmail.com>
2021-10-12 23:03:03 +0700

    menambahkan contoh1 php

---
+++ bcit-ci-CodeIgniter-b73eb19/application/controllers/contoh1.php
@@ -0,0 +1,15 @@
+<<?php 
+/**
+  * 
+  */
+ class contoh1 extends CI_Controller
+ {
+ 	
+ 	public function index()
+ 	{
+ 		echo "<h1> Perkenalkan</h1>" ;
+ 		echo"Nama saya dewi putri 
+ 		anak baik" ;
+ 		
+ 	}
+ } ?>
